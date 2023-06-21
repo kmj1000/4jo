5825b91bcdcf867ed05be1f740109b2d73020c1c
kmj1000 <rlaalswnoo6@gmail.com>
2023-06-21 14:29:30 +0900

    asd

--- src/main/java/com/java/servlet/CommunityServlet.java
+++ src/main/java/com/java/servlet/CommunityServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class CommunityServlet abcd
+ * Servlet implementation class CommunityServlet abcde
  */
 @WebServlet("/CommunityServlet")
 public class CommunityServlet extends HttpServlet {
