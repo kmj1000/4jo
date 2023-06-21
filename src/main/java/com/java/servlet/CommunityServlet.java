bdfa0e79f753fe13ea071f365e20fe028a7c34ca
2qiuo <2qiuo@naver.com>
2023-06-21 14:27:43 +0900

    예찌짱

--- src/main/java/com/java/servlet/CommunityServlet.java
+++ src/main/java/com/java/servlet/CommunityServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+	//예찌짱ㅇㅇ
 /**
  * Servlet implementation class CommunityServlet
  */
