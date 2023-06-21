8c4cef43a0a615fcbb5ee1521801bf6a4288ba4e
2qiuo <2qiuo@naver.com>
2023-06-21 16:18:03 +0900

    vo 추가 맘에 안 들면 수정해조 -예지-

--- src/main/java/com/java/servlet/vo/CommunityVO.java
+++ src/main/java/com/java/servlet/vo/CommunityVO.java
@@ -1,5 +1,49 @@
 package com.java.servlet.vo;
 
-public class CommunityVO {
+import java.util.Date;
 
+public class CommunityVO {
+	private int c_no;
+    private String nickname;
+    private String title;
+    private String content;
+    private Date reg_date;
+	public int getC_no() {
+		return c_no;
+	}
+	public void setC_no(int c_no) {
+		this.c_no = c_no;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+	@Override
+	public String toString() {
+		return "CommunityVO [c_no=" + c_no + ", nickname=" + nickname + ", title=" + title + ", content=" + content
+				+ ", reg_date=" + reg_date + "]";
+	}
+    
+    
 }
--- src/main/java/com/java/servlet/vo/Favorite_S_VO.java
+++ src/main/java/com/java/servlet/vo/Favorite_S_VO.java
@@ -1,5 +1,56 @@
 package com.java.servlet.vo;
 
 public class Favorite_S_VO {
-
+	private int favorite_shelter_no;
+	private int shelter_no;
+    private String nickname;
+    private String careNmShelter;
+    private String careAddrShelter;
+    private String careTelShelter;
+    
+	public int getFavorite_shelter_no() {
+		return favorite_shelter_no;
+	}
+	public void setFavorite_shelter_no(int favorite_shelter_no) {
+		this.favorite_shelter_no = favorite_shelter_no;
+	}
+	public int getShelter_no() {
+		return shelter_no;
+	}
+	public void setShelter_no(int shelter_no) {
+		this.shelter_no = shelter_no;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getCareNmShelter() {
+		return careNmShelter;
+	}
+	public void setCareNmShelter(String careNmShelter) {
+		this.careNmShelter = careNmShelter;
+	}
+	public String getCareAddrShelter() {
+		return careAddrShelter;
+	}
+	public void setCareAddrShelter(String careAddrShelter) {
+		this.careAddrShelter = careAddrShelter;
+	}
+	public String getCareTelShelter() {
+		return careTelShelter;
+	}
+	public void setCareTelShelter(String careTelShelter) {
+		this.careTelShelter = careTelShelter;
+	}
+	
+	@Override
+	public String toString() {
+		return "Favorite_S_VO [favorite_shelter_no=" + favorite_shelter_no + ", shelter_no=" + shelter_no
+				+ ", nickname=" + nickname + ", careNmShelter=" + careNmShelter + ", careAddrShelter=" + careAddrShelter
+				+ ", careTelShelter=" + careTelShelter + "]";
+	}
+    
+    
 }
