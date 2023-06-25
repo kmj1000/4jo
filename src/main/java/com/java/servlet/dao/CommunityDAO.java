ee885887ab1b97c37b64b6e009f6e7813fc66ee0
greenmurder <kki3103@gmail.com>
2023-06-25 17:53:47 +0900

    커뮤니티 노티스 수정

--- src/main/java/com/java/servlet/dao/CommunityDAO.java
+++ src/main/java/com/java/servlet/dao/CommunityDAO.java
@@ -1,7 +1,16 @@
 package com.java.servlet.dao;
 
+import java.util.List;
+
+import com.java.servlet.util.PageMaker;
 import com.java.servlet.vo.CommunityVO;
 
 public interface CommunityDAO {
 	CommunityVO selectCommunity(int c_no);
+
+	List<CommunityVO> selectAllCommunity();
+
+	int selectCountAllCommunity();
+
+	List<CommunityVO> selectAllCommunityByPage(PageMaker pageMaker);
 }
--- src/main/java/com/java/servlet/service/CommunityService.java
+++ src/main/java/com/java/servlet/service/CommunityService.java
@@ -1,7 +1,14 @@
 package com.java.servlet.service;
 
+import java.util.List;
+
+import com.java.servlet.util.PageMaker;
 import com.java.servlet.vo.CommunityVO;
 
 public interface CommunityService {
 	CommunityVO getCommunity(int c_no);
+
+	int getCountAllCommunity();
+
+	List<CommunityVO> getAllCommunityByPage(PageMaker pageMaker);
 }
